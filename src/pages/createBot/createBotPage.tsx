2dc3b59b00086e5df983ab445b857a863493d758
paul <den19980107@gmail.com>
2021-03-31 14:48:18 +0800

    新增 bot 時可以輸入 bot name

--- src/pages/createBot/createBotPage.tsx
+++ src/pages/createBot/createBotPage.tsx
@@ -1,8 +1,9 @@
-import { Button, message } from 'antd';
+import { Button, Input, message } from 'antd';
 import React, { useState } from 'react'
 import { ArrowHeadType, Elements, ReactFlowProvider, Node, Position } from 'react-flow-renderer'
 import { useQueryClient } from 'react-query';
 import { useCreateBot } from '../../api/bot';
+import history from '../../history';
 import { reactFlowToBotBuilderFlow } from '../../utils/ReactFlow_BotBuilderFlow_Converter';
 import { FlowElementsContext } from './flowElementsContext';
 import { ReactFlowContainer } from './reactFlowContainer';
@@ -12,6 +13,7 @@ interface CreateBotPageProps {
 }
 
 export const CreateBotPage: React.FC<CreateBotPageProps> = ({ }) => {
+    const [botName, setBotName] = useState<string>();
     const [elements, setElements] = useState<Elements<any>>([]);
     const [currentDragElement, setCurrentDragElement] = useState<Node | null>(null)
     const [currentSelectElement, setCurrentSelectElement] = useState<Node | null>(null)
@@ -27,15 +29,22 @@ export const CreateBotPage: React.FC<CreateBotPageProps> = ({ }) => {
         // TODO 建立一個 context provider 讓這些 component 都能存取節點(elements)的 state
         <ReactFlowProvider>
             <FlowElementsContext.Provider value={{ elements, setElements, currentDragElement, setCurrentDragElement, currentSelectElement, setCurrentSelectElement }}>
+                <div style={{ display: "flex", marginBottom: "1rem" }}>
+                    <Input onChange={(e) => setBotName(e.target.value)} placeholder="請輸入 bot 名稱"></Input>
+                    <Button onClick={() => {
+                        const script = reactFlowToBotBuilderFlow(elements)
+                        if (botName) {
+                            createBotMutation({
+                                name: botName,
+                                script: JSON.stringify(script)
+                            })
+                            history.push("/")
+                        } else {
+                            message.error("請輸入 bot 名稱！")
+                        }
+                    }}>新增 bot</Button>
+                </div>
                 <ReactFlowContainer></ReactFlowContainer>
-                <Button onClick={() => {
-                    const script = reactFlowToBotBuilderFlow(elements)
-                    console.log(script)
-                    createBotMutation({
-                        name: "test",
-                        script: JSON.stringify(script)
-                    })
-                }}>新增 bot</Button>
             </FlowElementsContext.Provider>
         </ReactFlowProvider>
 
