6d10172975e6ea46b0f6028980fe0155b9fe9130
paul <den19980107@gmail.com>
2021-04-09 16:24:01 +0800

    加入更新 bot 頁面

--- src/container/content.router.tsx
+++ src/container/content.router.tsx
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import { HomePage } from '../pages/home'
 import { CreateBotPage } from '../pages/createBot'
 import { NotfoundPage } from '../pages/notfound'
+import { UpdateBotPage } from '../pages/updateBot';
 
 interface ContentRouterProps {
 
@@ -15,6 +16,7 @@ const ContentRouter: React.FC<ContentRouterProps> = ({ }) => {
         <Switch>
             <Route exact path="/" component={HomePage} />
             <Route exact path="/bot/create" component={CreateBotPage}></Route>
+            <Route exact path="/bot/update/:id" component={UpdateBotPage}></Route>
             <Route exact component={NotfoundPage} />
         </Switch>
     )
---
+++ src/pages/updateBot/updateBotPage.tsx
@@ -0,0 +1,59 @@
+import { message } from 'antd';
+import React, { useEffect } from 'react'
+import { useQueryClient } from 'react-query';
+import { useParams } from 'react-router';
+import { useBotData, useUpdateBot } from '../../api/bot';
+import queryKeys from '../../api/queryKeys';
+import { FlowDesigner } from '../../components/FlowDesigner';
+import history from '../../history';
+
+interface ParamTypes {
+    id: string
+}
+
+interface UpdateBotPageProps {
+
+}
+
+export const UpdateBotPage: React.FC<UpdateBotPageProps> = ({ }) => {
+    const { id } = useParams<ParamTypes>();
+    const { data, isLoading, error } = useBotData(id)
+
+    const queryClient = useQueryClient();
+    const updateBotMutation = useUpdateBot({
+        onSuccess: () => {
+            queryClient.invalidateQueries(queryKeys.user.BOTS);
+            queryClient.invalidateQueries(queryKeys.bot.BOT)
+            message.success("更新成功！")
+            history.push("/")
+        }
+    })
+
+    /**
+     * 如果需要 react query 每次進來都重新 fetch 一次，要先把他儲存的 remove 掉 才不會拿到舊的資料
+     */
+    useEffect(() => {
+        return () => {
+            queryClient.removeQueries(queryKeys.bot.BOT);
+        }
+    }, [])
+
+    if (isLoading) return <div>loading...</div>
+    if (error) return <div>error</div>
+    if (data && data.nodes) {
+        const nodes = JSON.parse(data.nodes)
+        return (
+            <FlowDesigner initialbotname={data.name} initialElements={nodes} onSave={(name, elements) => {
+                console.log(name, elements)
+                updateBotMutation({
+                    name: name,
+                    nodes: JSON.stringify(elements),
+                    botId: id
+                })
+            }}></FlowDesigner>
+        )
+    }
+    return (
+        <div>error</div>
+    );
+}
